e85fe9876e60b896844acd092aa52e62d2fdce9b
Abdelhak <abdelhakrabhi0@gmail.com>
2022-03-19 14:41:27 +0100

    fourth day

--- src/components/SpecialMenu/SpecialMenuItem.js
+++ src/components/SpecialMenu/SpecialMenuItem.js
@@ -1,58 +1,55 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 //local Imports
-import {miniMobile} from '../../services/Responsive';
+import { miniMobile } from "../../services/Responsive";
 
-
-function SpecialMenuItem({title, price, tags}) {
+function SpecialMenuItem({ title, price, tags }) {
   return (
     <Wrapper>
-    <Info>
-      <Title>{title}</Title>
-      <Separator></Separator>
-      <Price>{price}</Price>
-    </Info>
-    <Tags>{tags}</Tags>
+      <Info>
+        <Title>{title}</Title>
+        <Separator></Separator>
+        <Price>{price}</Price>
+      </Info>
+      <Tags>{tags}</Tags>
     </Wrapper>
-  )
+  );
 }
 
-export default SpecialMenuItem
+export default SpecialMenuItem;
 
 const Wrapper = styled.article`
-margin: 40px 0;
+  margin: 40px 0;
 `;
 
-
 const Info = styled.div`
-display: flex;
-align-items: center;
-${miniMobile({flexDirection:'column', alignItems:'center', gap: '10px'})}
-`
+  display: flex;
+  align-items: center;
+  ${miniMobile({ flexDirection: "column", alignItems: "center", gap: "10px" })}
+`;
 
 const Title = styled.h3`
-flex: 1;
-color: var(--color-gold);
-font-size: 20px;
-font-weight: 700;
-`
+  flex: 1;
+  color: var(--color-gold);
+  font-size: 20px;
+  font-weight: 700;
+`;
 
 const Tags = styled.p`
-color: var(--color-grey);
-font-size: 13px;
-margin-top: 10px;
-font-family: var(--font-openSans);
-${miniMobile({textAlign: 'center', width: '100%'})};
+  color: var(--color-grey);
+  font-size: 13px;
+  margin-top: 10px;
+  font-family: var(--font-openSans);
+  ${miniMobile({ textAlign: "center", width: "100%" })};
 `;
 
 const Separator = styled.div`
-width: 100px;
-height: 3px;
-margin: 0 30px;
-background: var(--color-gray);
-
-`
+  width: 100px;
+  height: 3px;
+  margin: 0 30px;
+  background: var(--color-gray);
+`;
 const Price = styled.p`
-color: var(--color-white);
-`
+  color: var(--color-white);
+`;
